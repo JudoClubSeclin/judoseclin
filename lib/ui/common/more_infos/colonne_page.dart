fd090d7a8427ec0f7863ec7db1ad9e391f018cdb
pickypou <ludo.spysschaert@orange.fr>
2023-09-05 18:16:37 +0200

    chargement des pdf et ajustement des colonnes

--- lib/ui/common/more_infos/colonne_page.dart
+++ lib/ui/common/more_infos/colonne_page.dart
@@ -6,7 +6,6 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:judoseclin/ui/common/more_infos/oriented_size_box.dart';
 import 'package:judoseclin/ui/common/theme.dart';
 
-
 import 'package:url_launcher/url_launcher.dart';
 
 class ColonnePage extends HookWidget {
@@ -14,45 +13,35 @@ class ColonnePage extends HookWidget {
   final double fraction;
   final Size size;
   const ColonnePage(
-      {
-    Key? key,
-        required this.docUrl,
-        required this.fraction,
-        required this.size
-        }): super(key: key);
+      {Key? key,
+      required this.docUrl,
+      required this.fraction,
+      required this.size})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final contenu = useState<String>("...");
-    rootBundle.loadString(docUrl).then((value){
+    rootBundle.loadString(docUrl).then((value) {
       contenu.value = value;
     });
 
     return OrientedSizedBox(
       size: size,
       fraction: fraction,
-      child:  Card(
-
-        color: Colors.white,
-        child: SizedBox(
-         height: MediaQuery.sizeOf(context).height *1.33,
-
+      child: Card(
         child: Padding(
-
           padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 10),
           child: MarkdownBody(
-            data: contenu.value,
-            styleSheet: getMDTheme(context, Colors.black),
-            onTapLink: (text, url, title){
-              if(url != null){
-                launchUrl(Uri.parse(url));
-    }
-              }
-
-          ),
+              data: contenu.value,
+              styleSheet: getMDTheme(context, Colors.black),
+              onTapLink: (text, url, title) {
+                if (url != null) {
+                  launchUrl(Uri.parse(url));
+                }
+              }),
         ),
       ),
-      )
     );
   }
 }
