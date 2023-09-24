cd06c9c83bddfba23d1d91464977508ffb53e7da
pickypou <ludo.spysschaert@orange.fr>
2023-09-24 18:10:51 +0200

    corection

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-//void main() {
+void main() {
 // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 // Build our app and trigger a frame.
-//await tester.pumpWidget(const Landing());
+await tester.pumpWidget(const Landing());
 
 // Verify that our counter starts at 0.
 //expect(find.text('0'), findsOneWidget);
