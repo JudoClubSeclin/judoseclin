af7bd365de6e79d3688b95fedf3450c09f19a2dc
pickypou <ludo.spysschaert@orange.fr>
2024-02-13 07:26:43 +0100

    reprise du code Account création d'repository

--- lib/domain/usecases/user/fetch_user_data.dart
+++ lib/domain/usecases/user/fetch_user_data.dart
@@ -1,15 +1,11 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:judoseclin/data/repository/user_repository/user_repository.dart';
 
 class FetchUserDataUseCase {
+  final UsersRepository usersRepository;
+
+  FetchUserDataUseCase(this.usersRepository);
+
   Future<Map<String, dynamic>> invoke() async {
-    final currentUser = FirebaseAuth.instance.currentUser;
-    final firestore = FirebaseFirestore.instance;
-    if (currentUser != null) {
-      DocumentSnapshot snapshot =
-          await firestore.collection('Users').doc(currentUser.uid).get();
-      return snapshot.data() as Map<String, dynamic>;
-    }
-    throw Exception('No current user found.');
+    return await usersRepository.fetchUserData();
   }
 }
