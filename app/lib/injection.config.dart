98d7f4a2e9b67df041d345f5ceae35b1336fac21
Benoît FONTAINE <benoit.fontaine@septeo.com>
2024-06-27 14:13:58 +0200

    fix: upgrade packages & injection

--- app/lib/injection.config.dart
+++ app/lib/injection.config.dart
@@ -11,8 +11,8 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'data/repository/user_repository/auth_user.dart' as _i3;
-import 'data/repository/user_repository/user_info.dart' as _i4;
+import 'data/repository/user_repository/auth_user.dart' as _i4;
+import 'data/repository/user_repository/user_info.dart' as _i3;
 import 'data/repository/user_repository/user_is_connected.dart' as _i5;
 
 extension GetItInjectableX on _i1.GetIt {
@@ -26,10 +26,10 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.singleton<_i3.AuthUserRepository>(_i3.AuthUserRepositoryImpl());
-    gh.factory<_i4.UserInfoRepository>(() => _i4.UserInfoRepositoryImpl());
+    gh.factory<_i3.UserInfoRepository>(() => _i3.UserInfoRepositoryImpl());
+    gh.singleton<_i4.AuthUserRepository>(() => _i4.AuthUserRepositoryImpl());
     gh.singleton<_i5.UserIsConnectedRepository>(
-        _i5.UserIsConnectedRepositoryImpl());
+        () => _i5.UserIsConnectedRepositoryImpl());
     return this;
   }
 }
