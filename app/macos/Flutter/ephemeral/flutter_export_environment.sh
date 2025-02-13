e95bcc2cd9a2668ee1416dbd196092b4305b8cdc
pickypou <ludo.spysschaert@orange.fr>
2025-02-13 19:19:37 +0100

    test

---
+++ app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/3.13.0/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ludovic.spysschaert/Documents/Developpements/judoseclin/app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=18"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
