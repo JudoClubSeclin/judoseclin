fd488c41e15b44a7006ca33772f6888203a7ddcd
Benoît FONTAINE <benoit.fontaine@septeo.com>
2025-09-11 19:45:19 +0200

    fix: manage new/old features by kDebugMode

--- app/lib/ui/landing/landing_module.dart
+++ app/lib/ui/landing/landing_module.dart
@@ -1,8 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:injectable/injectable.dart';
 import 'package:judoseclin/ui/ui_module.dart';
 import 'package:landing_page/landing_page.dart' deferred as landing_page;
+
+import '../../core/di/api/firestore_service.dart';
+import '../../core/di/injection.dart';
+import '../../data/repository/competition_repository_impl.dart';
+import 'landing.dart';
 
 @singleton
 class LandingModule implements UIModule {
@@ -20,37 +26,40 @@ class LandingModule implements UIModule {
   @override
   List<RouteBase> getRoutes() {
     return [
-      // GoRoute(
-      //   path: '/',
-      //   builder:
-      //       (context, state) => Landing(
-      //         competitionRepository: CompetitionRepositoryImpl(
-      //           getIt<FirestoreService>(),
-      //         ),
-      //       ),
-      // ),
-      GoRoute(
-        path: '/',
-        builder: (context, state) => FutureBuilder(
-          future: () async {
-            await landing_page.loadLibrary();
-            await landing_page.WithImageOnly.precacheImages(context);
-          }(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return landing_page.WithImageOnly();
-            } else {
-              return const Center(
-                child: SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: CircularProgressIndicator(),
+      ?(kDebugMode)
+          ? null
+          : GoRoute(
+              path: '/',
+              builder: (context, state) => Landing(
+                competitionRepository: CompetitionRepositoryImpl(
+                  getIt<FirestoreService>(),
                 ),
-              );
-            }
-          },
-        ),
-      ),
+              ),
+            ),
+      ?(kDebugMode)
+          ? GoRoute(
+              path: '/',
+              builder: (context, state) => FutureBuilder(
+                future: () async {
+                  await landing_page.loadLibrary();
+                  await landing_page.WithImageOnly.precacheImages(context);
+                }(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return landing_page.WithImageOnly();
+                  } else {
+                    return const Center(
+                      child: SizedBox(
+                        height: 50,
+                        width: 50,
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }
+                },
+              ),
+            )
+          : null,
     ];
   }
 
